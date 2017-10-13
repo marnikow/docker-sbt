6837b91d83692c8551f44db6ecfc828647eb11d4
Cesar Perez <cperez@wnohang.com>
2017-10-13 18:10:53 +0200

    Updated the generations script to include the changes in underlying Scala image

--- update.sh
+++ update.sh
@@ -3,9 +3,11 @@
 set -eo pipefail
 
 declare -a sbt_versions=(
+  1.0.2
+  1.0.1
+  1.0.0
   0.13.16
   0.13.15
-  0.13.13
 )
 
 declare -a scala_versions=(
@@ -17,22 +19,45 @@ declare -a scala_versions=(
   2.11.10
   2.11.9
   2.11.8
+  2.11.7
+  2.11.6
+  2.11.5
+  2.11.4
+  2.11.3
+  2.11.2
+  2.11.1
+  2.11.0
   2.10.6
   2.10.5
   2.10.4
   2.10.3
+  2.10.2
 )
+
+generate_standard() {
+  echo "Generating Dockerfile for SBT v$1 (Scala v$2)"
+  mkdir -p "$1/$2"
+
+  sed -e "s/<sbt-version>/$1/g" dockerfile/standard.template \
+  | sed -e "s/<scala-version>/$2/g" - > "$1/$2/Dockerfile"
+}
+
+generate_alpine() {
+  echo "Generating Dockerfile for alpine SBT v$1 (Scala v$2)"
+  mkdir -p "$1/alpine/$2"
+
+  sed -e "s/<sbt-version>/$1/g" dockerfile/alpine.template \
+  | sed -e "s/<scala-version>/$2/g" - > "$1/alpine/$2/Dockerfile"
+}
 
 for sbt_version in "${sbt_versions[@]}"
 do
+  rm -rf "${sbt_version}"
+  mkdir -p "${sbt_version}"
+
   for scala_version in "${scala_versions[@]}"
   do
-    dirname="${sbt_version}/${scala_version}"
-
-    rm -rf "$dirname"
-    mkdir -p "$dirname"
-
-    sed -e "s/<sbt-version>/$sbt_version/g" Dockerfile.template \
-    | sed -e "s/<scala-version>/$scala_version/g" - > "$dirname/Dockerfile"
+    generate_standard "${sbt_version}" "${scala_version}"
+    generate_alpine "${sbt_version}" "${scala_version}"
   done
 done
